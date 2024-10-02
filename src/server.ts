2d41ebf8b2015109e71b69efd80e57cb67444032
RodrigoKloeckner <rodrigo.kloeckner@gmail.com>
2024-10-02 09:27:22 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('http server running')
+  })
